6a1e5662366031ec78ecbbac88b11adf115f324a
Roman Golyshev <anstreth@gmail.com>
2020-08-16 18:21:21 +0300

    Refactor code in index.ts

--- index.ts
+++ index.ts
@@ -15,15 +15,21 @@ express()
   .get('/', wrap(render_slides))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+function extract_base_address(url: string): string | undefined {
+  const last_slash = url.lastIndexOf('/');
+  return last_slash != -1 ? url.substring(0, last_slash) : undefined;
+}
+
 async function render_slides(req: express.Request, res: express.Response) {
-  const result = await request(req.query.file);
-  const last_slash = req.query.file.lastIndexOf('/');
-  const base_address = last_slash != -1 ? req.query.file.substring(0, last_slash) : undefined;
+  const slides_url = req.query.url
+  
+  const result = await request(slides_url);
+  const base_address = extract_base_address(slides_url);
 
   res.send(conver_ascii_doc_to_slides(result, base_address));
 }
 
-function conver_ascii_doc_to_slides(adoc: string, imagesdir: string): string {
+function conver_ascii_doc_to_slides(adoc: string, imagesdir?: string): string {
   const options = {
     safe: 'safe', 
     backend: 'revealjs', 
